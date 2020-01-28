caa170bfb28a3b086973458982f1e656c8de6f12
kilometers <kilo@meters.sh>
2020-01-28 00:08:03 -0800

    Respect global ChannelID value in livestream page

--- pkg/livestream/livestream.go
+++ pkg/livestream/livestream.go
@@ -3,11 +3,22 @@ package livestream
 import (
 	"net/http"
 	"text/template"
+
+	"github.com/kil0meters/acolyte/pkg/homepage"
 )
 
 var livestreamTemplate *template.Template = template.Must(template.ParseFiles("./templates/livestream.html"))
 
+// Data data for livestream page
+type Data struct {
+	ChannelID string
+}
+
 // ServeLivestream serves livestream page
 func ServeLivestream(w http.ResponseWriter, r *http.Request) {
-	livestreamTemplate.Execute(w, nil)
+	data := Data{
+		ChannelID: homepage.YoutubeChannelID,
+	}
+
+	livestreamTemplate.Execute(w, data)
 }
