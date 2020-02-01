1288d10d786a2b75778c496b640097265bb9a662
kilometers <kilo@meters.sh>
2020-02-01 00:56:27 -0800

    Refactor some chat code

--- acolyte-web/scripts/chat.js
+++ acolyte-web/scripts/chat.js
@@ -1,159 +1,171 @@
-var conn = new WebSocket('ws://localhost:8080/api/v1/chat')
-messageListElement = document.getElementById('message-list')
-username = "username"
-settingsShown = false
-loginPromptShown = false
-isUnauthroized = true
-const entryBody = document.getElementById('entry-body')
+class MessageList {
+  constructor(messageListElement, maxHeight) {
+    this._list = []
+    this.messageListElement = messageListElement
+    this.maxHeight = maxHeight
+  }
 
-var chatCommands = ["/ban", "/mute"]
+  static buildMessage(message) {
+    let messageElement = document.createElement("div")
+    messageElement.id = message.id
+    messageElement.classList.add("chat-message")
 
-messageList = {
-  _list: [],
-  addMessageListener: function(message) {},
-  removeMessageListener: function(id) {},
+    let usernameElement = document.createElement("a")
+    usernameElement.href = '#' + message.username
+    usernameElement.textContent = message.username
+    usernameElement.classList.add("message-username")
+
+    let textElement = document.createElement("span")
+    textElement.textContent = message.text
+    textElement.classList.add("message-text")
+
+    messageElement.appendChild(usernameElement)
+    messageElement.appendChild(textElement)
+    // messageElement.appendChild(messageElement)
+
+    return messageElement
+  }
+
   push(message) {
     this._list.push(message)
-    this.addMessageListener(message)
-  },
-  remove(id) {
+
+    let messageElement = MessageList.buildMessage(message)
+    this.messageListElement.appendChild(messageElement)
+
+    renderMathInElement(messageElement,{delimiters: [
+      {left: "$$", right: "$$", display: true},
+      {left: "$", right: "$", display: false}
+    ]})
+
+    if ((window.innerHeight + window.scrollY + 64) >= (document.body.offsetHeight - messageElement.offsetHeight)) {
+      window.scrollTo(0,document.body.scrollHeight)
+
+      while ((this.messageListElement.offsetHeight + 24) > this.maxHeight) { // offset for padding
+        this.removeByIndex(0)
+      }
+    }
+  }
+
+  removeByIndex(index) {
+    this._list.splice(index, 1)
+    this.messageListElement.children[0].remove()
+  }
+
+  removeByID(id) {
     for (i = 0; i < this._list.length; i++) {
       if (this._list[i].id === id) {
         this._list.splice(i, 1)
-        this.removeMessageListener(id)
+        this.messageListElement.removeChild(document.getElementById(id))
         break
       }
     }
-  },
+  }
+
   get list() { 
     return this._list
-  },
+  }
 }
 
-function buildMessage(message) {
-  let messageElement = document.createElement("div")
-  messageElement.id = message.id
-  messageElement.classList.add("chat-message")
+class MBChat {
+  constructor(maxHeight, noEntry) { 
+    this.maxHeight = maxHeight
+    this.conn = new WebSocket('ws://localhost:8080/api/v1/chat')
+    this.username = "username"
+    this.isUnauthroized = true
+    this.entryBody = document.getElementById('entry-body')
 
-  let usernameElement = document.createElement("a")
-  usernameElement.href = '#' + message.username
-  usernameElement.textContent = message.username
-  usernameElement.classList.add("message-username")
+    this.chatCommands = ["/ban", "/mute"]
+    this.timeoutInterval = null
 
-  let textElement = document.createElement("span")
-  textElement.textContent = message.text
-  textElement.classList.add("message-text")
+    this.messageList = new MessageList(document.getElementById('message-list'), this.maxHeight)
 
-  messageElement.appendChild(usernameElement)
-  messageElement.appendChild(textElement)
-  // messageElement.appendChild(messageElement)
-
-  return messageElement
-}
-
-messageList.addMessageListener = function (message) {
-  // messageListElement.children = []
-  // for (let message in value) {
-  
-  messageElement = buildMessage(message)
-
-  messageListElement.appendChild(messageElement)
-
-  console.log('window.scrollY: ' + (window.innerHeight + window.scrollY));
-  console.log('document.body.scrollHeight: ' + document.body.scrollHeight);
-  console.log('document.body.offsetHeight: ' + document.body.offsetHeight);
-
-  renderMathInElement(messageElement,{delimiters: [
-    {left: "$$", right: "$$", display: true},
-    {left: "$", right: "$", display: false}
-  ]})
-
-  if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - messageElement.offsetHeight)) {
-    window.scrollTo(0,document.body.scrollHeight)
+    if (noEntry != true) { 
+      this.initializeEntryBody()
+    }
   }
 
-}
+  initializeConnection() {
+    this.conn.addEventListener("message", (m) => {
+      console.log("sending message")
 
-messageList.removeMessageListener = function (id) {
-  messageListElement.removeChild(document.getElementById(id))
-}
+      if (m.data == "UNAUTHORIZED") {
+        this.isUnauthroized = true
+      }
+      else {
+        this.isUnauthroized = false
 
-// {"username": "<USERNAME>", "text": "<TEXT>"}
+        let data = JSON.parse(m.data)
 
-function initializeConnection(_conn) {
-  _conn.addEventListener("message", (m) => {
-    // console.log(m)
+        this.messageList.push(data)
+      }
+    })
 
-    if (m.data == "UNAUTHORIZED") {
-      isUnauthroized = true
-    }
-    else {
-      isUnauthroized = false
-      data = JSON.parse(m.data)
-      console.log(data)
-      // messageData = JSON.parse(data.body)
+    this.conn.addEventListener("close", (m) => {
+      this.messageList.push({ "username": "Client", "text": "Disconnected. Trying to reconnect in 5 seconds..."})
+      this.timeoutInterval = setInterval(() => {
+        console.log("Trying to reconnect")
+        this.messageList.push({ "username": "Client", "text": "Disconnected. Trying to reconnect in 5 seconds..."})
 
-      // console.log(messageData);
-      messageList.push(data)
-    }
-  })
+        this.conn = new WebSocket('ws://localhost:8080/api/v1/chat')
 
-  _conn.addEventListener("close", (m) => {
-    messageList.push({ "username": "Client", "text": "Disconnected. Trying to reconnect in 5 seconds..."})
-    interval = setInterval(function() {
-      console.log("Trying to reconnect")
-      messageList.push({ "username": "Client", "text": "Disconnected. Trying to reconnect in 5 seconds..."})
-      conn = new WebSocket('ws://localhost:8080/api/v1/chat')
+        setTimeout(() => {
+          if (this.conn.readyState == this.conn.OPEN) {
+            this.messageList.push({ "username": "Client", "text": "Reconnected."})
+            this.initializeConnection(this.conn)
+            console.log("Reconnected")
+            clearInterval(this.timeoutInterval)
+          }
+        }, 100)
+      }, 5000)
+    })
+  }
 
-      setTimeout(function () {
-        if (conn.readyState == conn.OPEN) {
-          messageList.push({ "username": "Client", "text": "Reconnected."})
-          initializeConnection(conn)
-          console.log("Reconnected")
-          clearInterval(interval)
+  initializeEntryBody() {
+    document.getElementById('entry-body').addEventListener("keyup", (event) => {
+      if (event.key == "Enter" && !event.shiftKey) {
+        if (this.isUnauthroized) {
+          toggleLoginPrompt()
+        } else {
+          this.conn.send(JSON.stringify({
+            "username": this.username,
+            "text": document.getElementById('entry-body').value,
+          }))
+          
+          document.getElementById('entry-body').value = ""
         }
-      }, 100)
-    }, 5000)
-  })
+      }
+    })
+  }
 }
 
-initializeConnection(conn)
+class Autocompletion {
+  constructor(entry) {
 
-document.getElementById('entry-body').addEventListener("keyup", function(event) {
-  if (event.key == "Enter" && !event.shiftKey) {
-    if (isUnauthroized) {
-      toggleLoginPrompt()
-    } else {
-      conn.send(JSON.stringify({
-        "username": username,
-        "text": entryBody.value,
-        }))
-      entryBody.value = ""
-    }
-  }
-})
-
-document.getElementById('entry-body').addEventListener("keyup", function(event) {
-  text = document.getElementById('entry-body').value
-
-  suggestions = []
-
-  for (autocompleteOption of chatCommands) {
-    if (autocompleteOption.startsWith(text)) {
-      suggestions.push(autocompleteOption)
-    }
   }
 
-  if (event.key == "Tab") {
-    
+  registerEventListeners() {
+    document.getElementById('entry-body').addEventListener("keyup", function(event) {
+      text = document.getElementById('entry-body').value
+  
+      suggestions = []
+  
+      for (autocompleteOption of chatCommands) {
+        if (autocompleteOption.startsWith(text)) {
+          suggestions.push(autocompleteOption)
+        }
+      }
+  
+      if (event.key == "Tab") {
+        
+      }
+  
+      console.log(suggestions)
+    })
   }
+}
 
-  console.log(suggestions);
-})
-
-document.getElementById('settings-username-input').addEventListener("keyup", function(event) {
-  username = document.getElementById('settings-username-input').value
-})
+loginPromptShown = false
+settingsShown = false
 
 function toggleLoginPrompt() {
   if (loginPromptShown) {
@@ -172,3 +184,4 @@ function toggleSettings() {
   }
   settingsShown = !settingsShown
 }
+
